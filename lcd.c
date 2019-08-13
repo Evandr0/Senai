1fe661a01962c528ae6e5a97a0e9958d367d4a8b
unknown <evandro_dias@saojose.sc.senai.br>
2019-08-12 21:45:59 -0300

    add lcd

---
+++ lcd.c
@@ -0,0 +1,25 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Hello, World!");
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print("Olah Mundo");
+  delay(10000);
+  lcd.clear();
+  lcd.print("Daqui a pouco");
+  lcd.setCursor(0, 1);
+  lcd.print("Vamos comecar");
+  delay(2000);
+  lcd.clear();
+  lcd.print("a brincar");
+  lcd.setCursor(0, 1);
+  lcd.print("Com variaveis!!");
+  delay(2000);
+  lcd.clear();
+}
+ 
