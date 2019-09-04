d34304ca1b9dec87ebbe5c88e79235c67774dfb9
unknown <evandro_dias@saojose.sc.senai.br>
2019-09-03 22:15:01 -0300

    add 4 novos exercicios

---
+++ Logica de programacao/14-_311.cpp
@@ -0,0 +1,77 @@
+/*Programa calcula a media de dois números com recuperação paralela.
+*/
+float ht, vh, pd, sb, desc, sl, td;
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+    Serial.print("\nDigite quantas horas trabalhou no Mes: ");
+    ht = le_numero_quebrado();
+    Serial.println(ht);
+    Serial.print("Digite quanto voce ganha por hora trabalhada: ");
+    vh = le_numero_quebrado();
+    Serial.println(vh);
+    Serial.print("Digite qual eh percentual de desconto: ");
+    pd = le_numero_quebrado();
+    Serial.println(pd);
+    sb = ht*vh;
+    td = (pd/100)*sb;
+    sl = sb - td;
+    Serial.print("horas trabalhadas: ");
+    Serial.println(ht);
+    Serial.print("Salario Bruto: ");
+    Serial.println(sb);
+    Serial.print("Desconto: ");
+    Serial.println(td);
+    Serial.print("Salario liquido: ");
+    Serial.println(sl);
+    Serial.println("===========================================");
+
+}
+
+int le_numero()
+{
+String conteudo = "";
+char caractere;
+int valor;
+while(Serial.available() == 0);
+// Enquanto receber algo pela serial
+while(Serial.available() > 0) {
+// Lê byte da serial
+caractere = Serial.read();
+// Ignora caractere de quebra de linha
+if (caractere != '\n'){
+// Concatena valores
+conteudo.concat(caractere);
+}
+// Aguarda buffer serial ler próximo caractere
+delay(10);
+}
+valor = conteudo.toInt(); 
+return valor;
+}
+
+float le_numero_quebrado()
+{
+String conteudo = "";
+char caractere;
+float valor;
+while(Serial.available() == 0);
+// Enquanto receber algo pela serial
+while(Serial.available() > 0) {
+// Lê byte da serial
+caractere = Serial.read();
+// Ignora caractere de quebra de linha
+if (caractere != '\n'){
+// Concatena valores
+conteudo.concat(caractere);
+}
+// Aguarda buffer serial ler próximo caractere
+delay(10);
+}
+valor = conteudo.toFloat(); 
+return valor;
+}
---
+++ Logica de programacao/14-_38.cpp
@@ -0,0 +1,76 @@
+/*Programa calcula a media de dois números com recuperação paralela.
+*/
+float corre1, corre2, corre3, corre4, corre5, soma;
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+    Serial.print("Digite o valor do primeiro 'corre': ");
+    corre1 = le_numero_quebrado();
+    Serial.println(corre1);
+    Serial.print("Digite o valor do segundo 'corre': ");
+    corre2 = le_numero_quebrado();
+    Serial.println(corre2);
+    Serial.print("Digite o valor do terceiro 'corre': ");
+    corre3 = le_numero_quebrado();
+    Serial.println(corre3);
+    Serial.print("Digite o valor do quarto 'corre': ");
+    corre4 = le_numero_quebrado();
+    Serial.println(corre4);
+    Serial.print("Digite o valor do quinto 'corre': ");
+    corre5 = le_numero_quebrado();
+    Serial.println(corre5);
+    soma = corre1+corre2+corre3+corre4+corre5;
+    Serial.print("O valor total das corridas foi: ");
+    Serial.println(soma);
+    Serial.println("=====================================");
+
+
+}
+
+int le_numero()
+{
+String conteudo = "";
+char caractere;
+int valor;
+while(Serial.available() == 0);
+// Enquanto receber algo pela serial
+while(Serial.available() > 0) {
+// Lê byte da serial
+caractere = Serial.read();
+// Ignora caractere de quebra de linha
+if (caractere != '\n'){
+// Concatena valores
+conteudo.concat(caractere);
+}
+// Aguarda buffer serial ler próximo caractere
+delay(10);
+}
+valor = conteudo.toInt(); 
+return valor;
+}
+
+float le_numero_quebrado()
+{
+String conteudo = "";
+char caractere;
+float valor;
+while(Serial.available() == 0);
+// Enquanto receber algo pela serial
+while(Serial.available() > 0) {
+// Lê byte da serial
+caractere = Serial.read();
+// Ignora caractere de quebra de linha
+if (caractere != '\n'){
+// Concatena valores
+conteudo.concat(caractere);
+}
+// Aguarda buffer serial ler próximo caractere
+delay(10);
+}
+valor = conteudo.toFloat(); 
+return valor;
+}
