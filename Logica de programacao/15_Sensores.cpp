381cc740e84bd6577880c8fd04737013020c6ea0
unknown <evandro_dias@saojose.sc.senai.br>
2019-09-09 20:35:45 -0300

    add leds

--- Logica de programacao/15_Sensores.cpp
+++ Logica de programacao/15_Sensores.cpp
@@ -5,6 +5,12 @@
 #define btn4 5
 #define btn5 6
 #define btn6 7
+#define led1 8
+#define led2 9
+#define led3 10
+#define led4 11
+#define led5 12
+#define led6 13
 
 int statusBtn1, statusBtn2, statusBtn3, statusBtn4;
 
@@ -18,17 +24,33 @@ void setup()
     pinMode(5, INPUT);
     pinMode(6, INPUT);
     pinMode(7, INPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
+
 }
 
 void loop()
 {
-    statusBtn1 =  digitalRead(btn1);
+    ler_sensores();
+    delay(100);
+
+  
+}
+
+void ler_sensores()
+{
+        statusBtn1 =  digitalRead(btn1);
     if (statusBtn1 == 1) {
         Serial.println("Tem coisa na gaveta 1...");
-               
-        statusBtn1 =1;
+        saida(led1, 1);
+
     }else {
         Serial.println("Gaveta 1 vazia");
+        saida(led1, 0);
     }
     delay(600);
     /*
@@ -38,42 +60,47 @@ void loop()
     statusBtn2 = digitalRead(btn2);
     if (statusBtn2 == 1) {
         Serial.println("Tem coisa na gaveta 2...");
-        
-        statusBtn2 =1;
+        saida(led2, 1);
+
     } else {
         Serial.println("Gaveta 2 vazia");
+        saida(led2, 0);
     }
     delay(600);
     statusBtn3 = digitalRead(btn3);
     if (statusBtn3 == 1) {
         Serial.println("Tem coisa na gaveta 3...");
-        
-        statusBtn3 =1;
+        saida(led3, 1);
+
     }else {
         Serial.println("Gaveta 3 vazia");
+        saida(led3, 0);
     }
     delay(600);
     statusBtn4 = digitalRead(btn4);
     if (statusBtn4 == 1) {
         Serial.println("Tem coisa na gaveta 4...");
-        
-        statusBtn4 =1;
+        saida(led4, 1);
+
     }else {
         Serial.println("Gaveta 4 vazia");
+        saida(led4, 0);
     }
     delay(600);
     if (digitalRead(btn5) == 1){
         Serial.println("Tem coisa na gaveta 5");
+        saida(led5, 1);
     } else {
         Serial.println("Gaveta 5 vazia");
+        saida(led5, 0);
     }
     delay(600);
     if (digitalRead(btn6) == 1) {
         Serial.println("Tem coisa na gaveta 6");
+        saida(led6, 1);
     }else {
         Serial.println("Gaveta 6 vazia");
+        saida(led6, 0);
     }
     delay(600);
-  
-  
 }
