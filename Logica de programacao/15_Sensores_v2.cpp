93a87c842548dcb81c4ecd9d48bfec535e889efa
unknown <evandro_dias@saojose.sc.senai.br>
2019-09-09 22:26:55 -0300

    add

--- Logica de programacao/15_Sensores_v2.cpp
+++ Logica de programacao/15_Sensores_v2.cpp
@@ -1,16 +1,19 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 #define saida digitalWrite
-#define btn1 2
-#define btn2 3
-#define btn3 4
-#define btn4 5
-#define btn5 6
-#define btn6 7
-#define led1 8
-#define led2 9
-#define led3 10
-#define led4 11
-#define led5 12
-#define led6 13
+#define btn1 6
+#define btn2 7
+#define btn3 8
+#define btn4 9
+#define btn5 10
+#define btn6 13
+#define led1 A0
+#define led2 A1
+#define led3 A2
+#define led4 A3
+#define led5 A4
+#define led6 A5
 
 int statusBtn1, statusBtn2, statusBtn3, statusBtn4;
 int gaveta1=0, gaveta2=0, gaveta3=0, gaveta4=0, gaveta5=0, gaveta6=0;
@@ -18,6 +21,7 @@ int gaveta1=0, gaveta2=0, gaveta3=0, gaveta4=0, gaveta5=0, gaveta6=0;
 
 void setup()
 {
+    lcd.begin(16, 2); //inicialiação do LCD
     Serial.begin(9600);
     pinMode(2, INPUT);
     pinMode(3, INPUT);
@@ -38,10 +42,43 @@ void loop()
 {
     ler_sensores();
     acionar_leds();
-
+    relatorio_serial();
+    relatorio_lcd();
     delay(100);
-
-  
+}
+void relatorio_serial(){
+    Serial.print("Gaveta1: ");
+    Serial.println(gaveta1);
+    Serial.print("Gaveta2: ");
+    Serial.println(gaveta2);
+    Serial.print("Gaveta3: ");
+    Serial.println(gaveta3);
+    Serial.print("Gaveta4: ");
+    Serial.println(gaveta4);
+    Serial.print("Gaveta5: ");
+    Serial.println(gaveta5);
+    Serial.print("Gaveta6: ");
+    Serial.println(gaveta6);
+}
+void relatorio_lcd(){
+    lcd.setCursor(0, 0);
+    lcd.print("G1:");
+    lcd.print(gaveta1);
+    lcd.print(" ");
+    lcd.print("G2:");
+    lcd.print(gaveta2);
+    lcd.print(" ");
+    lcd.print("G3:");
+    lcd.print(gaveta3);
+    lcd.setCursor(0, 1);
+    lcd.print("G4:");
+    lcd.print(gaveta4);
+    lcd.print(" ");
+    lcd.print("G5:");
+    lcd.print(gaveta5);
+    lcd.print(" ");
+    lcd.print("G6:");
+    lcd.print(gaveta6);
 }
 
 void acionar_leds()
