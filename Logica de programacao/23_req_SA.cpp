44fae27ab77db5177972a557b84acf4faf0c4637
evandr0 <evandro@live.jp>
2019-09-23 19:47:29 -0300

    alterado pasta

--- Logica de programacao/23_req_SA.cpp
+++ Logica de programacao/23_req_SA.cpp
@@ -5,14 +5,11 @@ void setup() {
 }
 
 void loop() {
-    Serial.println("=============================");
-    Serial.println("=====Escolha uma opcao: =====");
+    Serial.println("Escolha uma opcao:");
     Serial.println("opcao 1: Cadastrar produto;");
     Serial.println("opcao 2: Visualizar produto;");
-    Serial.println("opcao 3: Editar produto;");
-    Serial.println("opcao 4: Cadastrar funcionario;");
-    Serial.println("opcao 5: visualizar funcionario;");
-    Serial.println("=============================");
+    Serial.println("opcao 3: Cadastrar funcionario;");
+    Serial.println("opcao 4: visualizar funcionario;");
 
     menu = le_numero();
 
@@ -21,10 +18,8 @@ void loop() {
     } else if (menu == 2) {
         visualizar_produto();
     } else if (menu == 3) {
-        editar_produto();
-    }else if (menu == 4) {
         cadastrar_funcionario();
-    } else if (menu == 5) {
+    } else if (menu == 4) {
         visualizar_funcionario();
     }
     
@@ -34,13 +29,11 @@ String modeloProduto;
 String fabricante;
 String numeroDeSerie;
 String nomeFuncionario;
-String admissao;
 int matricula;
 int estoque;
 int quantidadeMinima;
 
 void cadastrar_produto(){
-    Serial.println("=============================\n====Opcao 1 foi escolhida====\n=============================");
     Serial.print("Digite o modelo do produto: ");
     modeloProduto = le_nome();
     Serial.println(modeloProduto);
@@ -56,13 +49,11 @@ void cadastrar_produto(){
     Serial.print("Digite a quantidade minima: ");
     quantidadeMinima = le_numero();
     Serial.println(quantidadeMinima);
-    Serial.println("============FIM==============");
 
 
 }
 
 void visualizar_produto(){
-    Serial.println("=============================\n====Opcao 2 foi escolhida====\n=============================");
     Serial.print("Modelo: ");
     Serial.println(modeloProduto);
     Serial.print("Fabricante: ");
@@ -72,69 +63,28 @@ void visualizar_produto(){
     Serial.print("Estoque: ");
     Serial.println(estoque);
     if (estoque <= quantidadeMinima) {
-        Serial.print("Estoque abaixo do esperado Qtd: ");
-        Serial.println(quantidadeMinima);
+        Serial.println("Estoque abaixo do esperado");
     } else {
-        Serial.print("Estoque normal Qtd: ");
-        Serial.println(estoque);
+        Serial.println("Estoque normal");
     }
-    Serial.println("============FIM==============");
-
-}
-
-void editar_produto(){
-    Serial.println("=============================\n====Opcao 3 foi escolhida====\n=============================");
-    Serial.println("Qual item deseja modificar?");
-    Serial.println("opcao 1: Modelo.");
-    Serial.println("opcao 2: Fabricante.");
-    Serial.println("opcao 3: Quantidade em estoque.");
-    Serial.println("opcao 4: Quanditdade minima permitida.");
-    menu = le_numero();
-    if (menu == 1){
-        Serial.print("Digite o novo Modelo: ");
-        modeloProduto = le_nome();
-        Serial.println(modeloProduto);
-    } else if (menu == 2) {
-        Serial.print("Digite o novo Fabricante: ");
-        fabricante = le_nome();
-        Serial.println(fabricante);
-    } else if (menu == 3) {
-        Serial.print("Digite a quantidade em estoque: ");
-        estoque = le_numero();
-        Serial.println(estoque);
-    }else if (menu == 4) {
-        Serial.print("Digite a quantidade minima permitida: ");
-        quantidadeMinima = le_numero();
-        Serial.println(quantidadeMinima);
-    }
-    Serial.println("============FIM==============");
 
 }
 
 void cadastrar_funcionario(){
-    Serial.println("=============================\n====Opcao 4 foi escolhida====\n=============================");
     Serial.print("Digite o nome do funcionario:");
     nomeFuncionario = le_nome();
     Serial.println(nomeFuncionario);
     Serial.print("Digite a matricula: ");
     matricula = le_numero();
     Serial.println(matricula);
-    Serial.print("Digite a data de admissão");
-    admissao = le_nome();
-    Serial.println(admissao);
-    Serial.println("============FIM==============");
 
 }
 
 void visualizar_funcionario(){
-    Serial.println("=============================\n====Opcao 5 foi escolhida====\n=============================");
     Serial.print("Nome do funcionario: ");
     Serial.println(nomeFuncionario);
     Serial.print("Matricula: ");
     Serial.println(matricula);
-    Serial.print("Admissao: ");
-    Serial.println(admissao);
-    Serial.println("============FIM==============");
 
 }
 /**
